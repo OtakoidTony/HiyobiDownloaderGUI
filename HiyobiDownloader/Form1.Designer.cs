e7fa0858a7fe54fef766be47391ee63ba892edbe
박현 <36785562+OtakoidTony@users.noreply.github.com>
2020-08-30 03:53:41 +0900

    upload

---
+++ HiyobiDownloader/Form1.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace HiyobiDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.debugTextBox = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            this.pictureBox13 = new System.Windows.Forms.PictureBox();
+            this.pictureBox14 = new System.Windows.Forms.PictureBox();
+            this.pictureBox15 = new System.Windows.Forms.PictureBox();
+            this.pageLabel = new System.Windows.Forms.Label();
+            this.gotoPrevPageButton = new System.Windows.Forms.Button();
+            this.gotoNextPageButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox14)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox15)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(497, 25);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(515, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(69, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // debugTextBox
+            // 
+            this.debugTextBox.BackColor = System.Drawing.SystemColors.Control;
+            this.debugTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.debugTextBox.Location = new System.Drawing.Point(752, 59);
+            this.debugTextBox.Multiline = true;
+            this.debugTextBox.Name = "debugTextBox";
+            this.debugTextBox.ReadOnly = true;
+            this.debugTextBox.Size = new System.Drawing.Size(362, 573);
+            this.debugTextBox.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 56);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(140, 190);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(160, 59);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(142, 187);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(308, 59);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(142, 187);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(456, 59);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(142, 187);
+            this.pictureBox4.TabIndex = 6;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(604, 59);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(142, 187);
+            this.pictureBox5.TabIndex = 7;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(12, 252);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(142, 187);
+            this.pictureBox6.TabIndex = 8;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Location = new System.Drawing.Point(160, 252);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(142, 187);
+            this.pictureBox7.TabIndex = 9;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Location = new System.Drawing.Point(308, 252);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(142, 187);
+            this.pictureBox8.TabIndex = 10;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Click += new System.EventHandler(this.pictureBox8_Click);
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Location = new System.Drawing.Point(456, 252);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(142, 187);
+            this.pictureBox9.TabIndex = 11;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.Click += new System.EventHandler(this.pictureBox9_Click);
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Location = new System.Drawing.Point(604, 252);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(142, 187);
+            this.pictureBox10.TabIndex = 12;
+            this.pictureBox10.TabStop = false;
+            this.pictureBox10.Click += new System.EventHandler(this.pictureBox10_Click);
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.Location = new System.Drawing.Point(12, 445);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(142, 187);
+            this.pictureBox11.TabIndex = 13;
+            this.pictureBox11.TabStop = false;
+            this.pictureBox11.Click += new System.EventHandler(this.pictureBox11_Click);
+            // 
+            // pictureBox12
+            // 
+            this.pictureBox12.Location = new System.Drawing.Point(160, 445);
+            this.pictureBox12.Name = "pictureBox12";
+            this.pictureBox12.Size = new System.Drawing.Size(142, 187);
+            this.pictureBox12.TabIndex = 14;
+            this.pictureBox12.TabStop = false;
+            this.pictureBox12.Click += new System.EventHandler(this.pictureBox12_Click);
+            // 
+            // pictureBox13
+            // 
+            this.pictureBox13.Location = new System.Drawing.Point(308, 445);
+            this.pictureBox13.Name = "pictureBox13";
+            this.pictureBox13.Size = new System.Drawing.Size(142, 187);
+            this.pictureBox13.TabIndex = 15;
+            this.pictureBox13.TabStop = false;
+            this.pictureBox13.Click += new System.EventHandler(this.pictureBox13_Click);
+            // 
+            // pictureBox14
+            // 
+            this.pictureBox14.Location = new System.Drawing.Point(456, 445);
+            this.pictureBox14.Name = "pictureBox14";
+            this.pictureBox14.Size = new System.Drawing.Size(142, 187);
+            this.pictureBox14.TabIndex = 16;
+            this.pictureBox14.TabStop = false;
+            this.pictureBox14.Click += new System.EventHandler(this.pictureBox14_Click);
+            // 
+            // pictureBox15
+            // 
+            this.pictureBox15.Location = new System.Drawing.Point(604, 445);
+            this.pictureBox15.Name = "pictureBox15";
+            this.pictureBox15.Size = new System.Drawing.Size(142, 187);
+            this.pictureBox15.TabIndex = 17;
+            this.pictureBox15.TabStop = false;
+            this.pictureBox15.Click += new System.EventHandler(this.pictureBox15_Click);
+            // 
+            // pageLabel
+            // 
+            this.pageLabel.AutoSize = true;
+            this.pageLabel.Location = new System.Drawing.Point(67, 8);
+            this.pageLabel.Name = "pageLabel";
+            this.pageLabel.Size = new System.Drawing.Size(19, 19);
+            this.pageLabel.TabIndex = 18;
+            this.pageLabel.Text = "1";
+            this.pageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.pageLabel.Click += new System.EventHandler(this.pageLabel_Click);
+            // 
+            // gotoPrevPageButton
+            // 
+            this.gotoPrevPageButton.Location = new System.Drawing.Point(3, 4);
+            this.gotoPrevPageButton.Name = "gotoPrevPageButton";
+            this.gotoPrevPageButton.Size = new System.Drawing.Size(49, 23);
+            this.gotoPrevPageButton.TabIndex = 19;
+            this.gotoPrevPageButton.Text = "<";
+            this.gotoPrevPageButton.UseVisualStyleBackColor = true;
+            this.gotoPrevPageButton.Click += new System.EventHandler(this.gotoPrevPageButton_Click);
+            // 
+            // gotoNextPageButton
+            // 
+            this.gotoNextPageButton.Location = new System.Drawing.Point(109, 4);
+            this.gotoNextPageButton.Name = "gotoNextPageButton";
+            this.gotoNextPageButton.Size = new System.Drawing.Size(49, 23);
+            this.gotoNextPageButton.TabIndex = 20;
+            this.gotoNextPageButton.Text = ">";
+            this.gotoNextPageButton.UseVisualStyleBackColor = true;
+            this.gotoNextPageButton.Click += new System.EventHandler(this.gotoNextPageButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.gotoPrevPageButton);
+            this.panel1.Controls.Add(this.gotoNextPageButton);
+            this.panel1.Controls.Add(this.pageLabel);
+            this.panel1.Location = new System.Drawing.Point(943, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(171, 34);
+            this.panel1.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1136, 695);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox15);
+            this.Controls.Add(this.pictureBox14);
+            this.Controls.Add(this.pictureBox13);
+            this.Controls.Add(this.pictureBox12);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.debugTextBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Hiyobi Downloader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox14)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox15)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox debugTextBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.PictureBox pictureBox12;
+        private System.Windows.Forms.PictureBox pictureBox13;
+        private System.Windows.Forms.PictureBox pictureBox14;
+        private System.Windows.Forms.PictureBox pictureBox15;
+        private System.Windows.Forms.Label pageLabel;
+        private System.Windows.Forms.Button gotoPrevPageButton;
+        private System.Windows.Forms.Button gotoNextPageButton;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ HiyobiDownloader/Form1.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RestSharp;
+using Newtonsoft.Json.Linq;
+
+namespace HiyobiDownloader
+{
+    public partial class Form1 : Form
+    {
+        
+        public static PictureBox[] pictureBoxes = new PictureBox[15];
+        public static int page = 1;
+        public static JObject loadedDatabase = null;
+        public static int selectedIndex = 0;
+        public static string selectedInfo = null;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBoxes[0] = pictureBox1;
+            pictureBoxes[1] = pictureBox2;
+            pictureBoxes[2] = pictureBox3;
+            pictureBoxes[3] = pictureBox4;
+            pictureBoxes[4] = pictureBox5;
+            pictureBoxes[5] = pictureBox6;
+            pictureBoxes[6] = pictureBox7;
+            pictureBoxes[7] = pictureBox8;
+            pictureBoxes[8] = pictureBox9;
+            pictureBoxes[9] = pictureBox10;
+            pictureBoxes[10] = pictureBox11;
+            pictureBoxes[11] = pictureBox12;
+            pictureBoxes[12] = pictureBox13;
+            pictureBoxes[13] = pictureBox14;
+            pictureBoxes[14] = pictureBox15;
+
+            foreach (PictureBox i in pictureBoxes) i.SizeMode = PictureBoxSizeMode.Zoom;
+            
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                page = 1;
+                pageLabel.Text = page + "";
+                start();
+            }
+        }
+
+        private void start()
+        {
+            String tags = textBox1.Text;
+            String[] tagsStringArray = Hiyobi.tagsToArray(tags);
+            String res = Hiyobi.search(tagsStringArray, page);
+            debugTextBox.Text = res;
+
+            loadedDatabase = JObject.Parse(res);
+            var test = "";
+            if (loadedDatabase["errorMsg"] != null)
+            {
+                debugTextBox.Text = (string)loadedDatabase["errorMsg"];
+                return;
+            }
+            foreach (var i in loadedDatabase["list"])
+            {
+                test += i["id"];
+                test += Environment.NewLine;
+            }
+            for (var i = 0; i < pictureBoxes.Length; i++)
+            {
+                pictureBoxes[i].SizeMode = PictureBoxSizeMode.Zoom;
+                try
+                {
+                    pictureBoxes[i].Load(Hiyobi.getCoverImageUrl((string)loadedDatabase["list"][i]["id"]));
+                }
+                catch (Exception)
+                {
+                    pictureBoxes[i].Image = null;
+                }
+
+            }
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            page = 1;
+            pageLabel.Text = page + "";
+            start();
+        }
+
+        private void printSelectedData(int selectedIndex)
+        {
+            selectedInfo = "";
+            JToken selectedData = loadedDatabase["list"][selectedIndex];
+            selectedInfo += "[id]" + Environment.NewLine;
+            selectedInfo += selectedData["id"] + Environment.NewLine + Environment.NewLine;
+            selectedInfo += "[title]" + Environment.NewLine;
+            selectedInfo += ((string)selectedData["title"]).Replace("&amp;", "&") + Environment.NewLine + Environment.NewLine;
+            selectedInfo += "[artists]" + Environment.NewLine;
+            foreach (var i in selectedData["artists"])
+            {
+                selectedInfo += i["value"] + Environment.NewLine;
+            }
+            selectedInfo += Environment.NewLine + "[groups]" + Environment.NewLine;
+            foreach (var i in selectedData["groups"])
+            {
+                selectedInfo += i["value"] + Environment.NewLine;
+            }
+            selectedInfo += Environment.NewLine + "[parodys]" + Environment.NewLine;
+            foreach (var i in selectedData["parodys"])
+            {
+                selectedInfo += i["value"] + Environment.NewLine;
+            }
+            selectedInfo += Environment.NewLine + "[characters]" + Environment.NewLine;
+            foreach (var i in selectedData["characters"])
+            {
+                selectedInfo += i["value"] + Environment.NewLine;
+            }
+            selectedInfo += Environment.NewLine + "[tags]" + Environment.NewLine;
+            foreach (var i in selectedData["tags"])
+            {
+                selectedInfo += i["value"] + Environment.NewLine;
+            }
+            debugTextBox.Text = selectedInfo;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            printSelectedData(0);
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            printSelectedData(1);
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            printSelectedData(2);
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            printSelectedData(3);
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            printSelectedData(4);
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            printSelectedData(5);
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            printSelectedData(6);
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+            printSelectedData(7);
+        }
+
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+            printSelectedData(8);
+        }
+
+        private void pictureBox10_Click(object sender, EventArgs e)
+        {
+            printSelectedData(9);
+        }
+
+        private void pictureBox11_Click(object sender, EventArgs e)
+        {
+            printSelectedData(10);
+        }
+
+        private void pictureBox12_Click(object sender, EventArgs e)
+        {
+            printSelectedData(11);
+        }
+
+        private void pictureBox13_Click(object sender, EventArgs e)
+        {
+            printSelectedData(12);
+        }
+
+        private void pictureBox14_Click(object sender, EventArgs e)
+        {
+            printSelectedData(13);
+        }
+
+        private void pictureBox15_Click(object sender, EventArgs e)
+        {
+            printSelectedData(14);
+        }
+
+        private void pageLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gotoPrevPageButton_Click(object sender, EventArgs e)
+        {
+            if (page == 1) return;
+            page -= 1;
+            pageLabel.Text = page+"";
+            start();
+        }
+
+        private void gotoNextPageButton_Click(object sender, EventArgs e)
+        {
+            page += 1;
+            pageLabel.Text = page + "";
+            start();
+        }
+    }
+
+    class Hiyobi
+    {
+        public static string[] tagsToArray(string tags)
+        {
+            return tags.Split(' ').Except(new List<string> { string.Empty }).ToArray();
+        }
+
+        private static string makeBody(string[] tags, int page)
+        {
+            var body = new JObject();
+            body.Add("search", JArray.FromObject(tags));
+            body.Add("paging", page);
+            return body.ToString();
+        }
+
+        public static string search(string[] tags, int page)
+        {
+            var client = new RestClient("https://api.hiyobi.me/search");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+
+            var body = makeBody(tags, page);
+            request.AddParameter("application/json", body, ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            return response.Content;
+        }
+
+        public static String getCoverImageUrl(int number)
+        {
+            return "https://cdn.hiyobi.me/tn/" + number + ".jpg";
+        }
+
+        public static String getCoverImageUrl(string number)
+        {
+            return "https://cdn.hiyobi.me/tn/" + number + ".jpg";
+        }
+    }
+}
